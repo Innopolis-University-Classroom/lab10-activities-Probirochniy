ac20715b752a480453893bbccac792b172e833f1
Damir Afliatonov <88425319+Probirochniy@users.noreply.github.com>
2022-03-28 14:30:27 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,53 @@
-// your solution here
-//fix all the errors including package errors
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class Burger {
+
+    private String cheese;
+    private String letucci;
+    private String tomato;
+    private String peperone;
+    private String size;
+
+   
+
+    public static class Builder {
+        private Burger newBurger;
+
+        public Builder() {
+            newBurger = new Burger();
+        }
+
+        public Builder withCheese(){
+            newBurger.cheese = "Added";
+            return this;
+        }
+
+        public Builder withTomato(){
+            newBurger.tomato = "Added";
+            return this;
+        }
+
+        public Builder withLetucci(){
+            newBurger.letucci = "Added";
+            return this;
+        }
+
+        public Builder withPeperone(){
+            newBurger.peperone = "Added";
+            return this;
+        }
+
+        public Builder withSize(String size){
+            newBurger.size = size;
+            return this;
+        }
+
+    }
+}

ca538aec8ae81a1e5c3cb9413fbf1aeb8f66c9bc
Damir Afliatonov <88425319+Probirochniy@users.noreply.github.com>
2022-03-28 13:58:37 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,32 @@
-// your solution here
-//fix all the errors including package errors
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+interface Shape{
+        void draw();
+}
+
+
+class Circle implements Shape{
+
+    @Override
+    public void draw() {
+    }
+}
+class Square implements Shape{
+
+    @Override
+    public void draw() {
+    }
+}
+class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+    }
+}
